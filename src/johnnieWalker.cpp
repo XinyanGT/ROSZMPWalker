b9c7f2f5772b28cfe5a555bcca3545af68a80ffc
ana-GT <ahuaman3@gatech.edu>
2013-05-02 16:42:15 -0400

    copy_right_version - walks a bit more freely

--- src/johnnieWalker.cpp
+++ src/johnnieWalker.cpp
@@ -172,25 +172,31 @@ int main( int argc, char** argv ) {
   right_support_kp.resize(28);
   right_support_ki.resize(28);
   
+  /*
   left_support_kp << default_kp, default_kp, default_kp, default_kp,
     default_kp,  strong_kp, 40, strong_kp, 40, strong_kp, 
     weak_kp, weak_kp, 40, weak_kp, 20, weak_kp,
     default_kp, default_kp, default_kp, default_kp, default_kp, default_kp,         
+    default_kp, default_kp, default_kp, default_kp, default_kp, default_kp;
+  */
+  left_support_kp << default_kp, default_kp, default_kp, default_kp,
+    strong_kp,  strong_kp, 40, strong_kp, 40, strong_kp, 
+    weak_kp, weak_kp, 40, weak_kp, 20, weak_kp,
     default_kp, default_kp, default_kp, default_kp, default_kp, default_kp,
-    
+    default_kp, default_kp, default_kp, default_kp, default_kp, default_kp;
     
     left_support_ki << default_ki, default_ki, default_ki, default_ki,
     strong_ki,  strong_ki, strong_ki, strong_ki, strong_ki, strong_ki, 
     weak_ki, weak_ki, weak_ki, weak_ki, weak_ki, weak_ki,
     default_ki, default_ki, default_ki, default_ki, default_ki, default_ki,
     default_ki, default_ki, default_ki, default_ki, default_ki, default_ki;
-  
+    
   right_support_kp << default_kp, default_kp, default_kp, default_kp,
     weak_kp, weak_kp, 40, weak_kp, 20, weak_kp,
     strong_kp,  strong_kp, 40, strong_kp, 40, strong_kp, 
     default_kp, default_kp, default_kp, default_kp, default_kp, default_kp,
     default_kp, default_kp, default_kp, default_kp, default_kp, default_kp;
-                  
+                
  
   right_support_ki << default_ki, default_ki, default_ki, default_ki,
     weak_ki, weak_ki, weak_ki, weak_ki, weak_ki, weak_ki,
